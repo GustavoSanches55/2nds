3abdf086a748ce8da7bdd6e0217cc6f7e0ad34c5
LucasTreuke <lucasbryantreuke@gmail.com>
2022-11-05 17:40:39 -0300

    inicio de estilização e template para models

--- silvia/base/models.py
+++ silvia/base/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
 # Create your models here.
+
+# create a class for the following tables (comentário criado para o copilot fazer pra mim 😉):
+# 1. alunos:
+# - id (int)
+# - nome (str)
+
+# 2. disciplinas:
+# - id (int)
+# - nome (str)
+# - professor (str)
+
+# 3. notas:
+# - id (int)
+# - aluno (int)
+# - id_disciplina (int)
+# - nota (float)
+
+
+class Aluno(models.Model):
+    nome = models.CharField(max_length=100)
+
+class Disciplina(models.Model):
+    nome = models.CharField(max_length=100)
+    professor = models.CharField(max_length=100)
+
+class Nota(models.Model):
+    aluno = models.ForeignKey(Aluno, on_delete=models.CASCADE)
+    disciplina = models.ForeignKey(Disciplina, on_delete=models.CASCADE)
+    nota = models.FloatField()
+
+
